7020070fe418636dc2cbbcadcb2c7c0e617ebab3
Brian Takita <brian.takita@gmail.com>
2023-10-10 22:07:24 -0400

    bun init
    + Astro:
    	+ Astro$_
    	+ Astro_
    	+ Astro__set
    	+ Astro__memo
    	+ Astro__memo_

---
+++ src/astro/Astro.ts
@@ -0,0 +1,11 @@
+import { type nullish } from '@ctx-core/function'
+import { val__be_atom_memo_tuple5_ } from '@ctx-core/solid-nanostores'
+import { type APIContext } from 'astro'
+export const [
+	Astro$_,
+	Astro_,
+	Astro__set,
+	Astro__memo,
+	Astro__memo_,
+] = val__be_atom_memo_tuple5_<APIContext|nullish>('Astro', ()=>
+	undefined)
